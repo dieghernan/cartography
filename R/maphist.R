3a954cb0a52e68ebe2aea9fbbcb4a81416fe1655
dieghernan <diego.hernangomezherrero@gmail.com>
2020-02-28 13:25:26 +0100

    Generalised histogram
    Next step is to check whether to include it on a map

--- R/maphist.R
+++ R/maphist.R
@@ -7,28 +7,34 @@ library(cartography)
 mtq <- st_read(system.file("gpkg/mtq.gpkg", package="cartography"))
 # Population density
 mtq$POPDENS <- 1e6 * mtq$POP / st_area(x = mtq)
+mtq$area=st_area(mtq)/1000000
+mtq$paro=mtq$CHOM/mtq$POP
 #Params
 x=mtq
-var="POPDENS"
-bks <- getBreaks(mtq$POPDENS, method = "quantile", nclass=15)
-cols <- carto.pal(pal1 = "wine.pal", length(bks)-1)
+var="area"
+
 #end
 
 
 values<-st_drop_geometry(x)
 values<-as.double(values[,var])
+bks <- getBreaks(values, method = "quantile", nclass=15)
+cols <- carto.pal(pal1 = "wine.pal", length(bks)-1)
 df<-data.frame(values)
 
 
+
 #Guess the breaks on hist
-bins<-pretty(df$values,n=50)
+hbreaks<-length(unique(values))
+bins<-pretty(df$values,n=hbreaks)
 bins<-pmax(bins,1.01*min(df$values))
 bins<-pmin(bins,max(df$values))
+
 vcol<-cut(bins,bks, include.lowest = TRUE, labels = FALSE)
 vcol<-as.data.frame(vcol)
 vcol$col=cols[vcol$vcol]
 
-hist(x=values, breaks=50,col = vcol$col,
+hist(x=values, breaks=hbreaks,col = vcol$col,
      axes=FALSE,xlab="", ylab = "", main = "", border = NA)
 axis(1, line=0.01, pos=NA, col=NA, col.ticks = "black")
 axis(2, line=0.01, tck=1, col=NA, col.ticks="grey95", lwd.ticks="1", lwd=0.1, lty=3)
