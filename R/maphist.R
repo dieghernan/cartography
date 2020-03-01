28bf502940f48e5dfc0b754159d982172dadf5c0
dieghernan <diego.hernangomezherrero@gmail.com>
2020-03-01 05:45:39 +0100

    Done, full function maphist

--- R/maphist.R
+++ R/maphist.R
@@ -1,43 +1,109 @@
-rm(list=ls())
-
-library(sf)
-library(cartography)
-
-
-mtq <- st_read(system.file("gpkg/mtq.gpkg", package="cartography"))
-# Population density
-mtq$POPDENS <- 1e6 * mtq$POP / st_area(x = mtq)
-mtq$area=st_area(mtq)/1000000
-mtq$paro=mtq$CHOM/mtq$POP
-#Params
-x=mtq
-var="area"
-
-#end
-
-
-values<-st_drop_geometry(x)
-values<-as.double(values[,var])
-bks <- getBreaks(values, method = "quantile", nclass=7)
-cols <- carto.pal(pal1 = "wine.pal", length(bks)-1)
-df<-data.frame(values)
-
-
-
-#Guess the breaks on hist
-hbreaks<-length(unique(values))
-bins<-pretty(df$values,n=hbreaks)
-bins<-pmax(bins,1.01*min(df$values))
-bins<-pmin(bins,max(df$values))
-
-vcol<-cut(bins,bks, include.lowest = TRUE, labels = FALSE)
-vcol<-as.data.frame(vcol)
-vcol$col=cols[vcol$vcol]
-
-hist(x=values, breaks=hbreaks,col = vcol$col,
-     axes=FALSE,xlab="", ylab = "", main = "", border = NA)
-axis(1, line=0.01, pos=NA, col=NA, col.ticks = "black")
-axis(2, line=0.01, tck=1, col=NA, col.ticks="grey95", lwd.ticks="1", lwd=0.1, lty=3)
-
-
-choroLayer(x,var = var, breaks = bks, col=cols)
+#' @title Histogram on maps
+#' @description Plot an histogram over a map.
+#' @name maphist
+#' @param v a vector of numeric values.
+#' @param breaks,method,nclass classification options, see \link{getBreaks}.
+#' @param col a vector of colors. 
+#' @param pos position of the legend, one of "topleft", "top", 
+#' "topright", "right", "bottomright", "bottom", "bottomleft", 
+#' "bottomleftextra", "left" or a vector of two coordinates in map units 
+#' (c(x, y)).
+#' @param hist.width,hist.height width and height of the histogram, in normalized device coordinates (NDC). See Details. 
+#' @param axes whether to add a horizontal (\code{"h"}), vertical (\code{"v"}) or both (\code{TRUE}) axes.
+#' @param axes.cex size of the values in the axes.
+#' @param frame whether to add a frame to the legend (\code{TRUE}) or not (\code{FALSE}).
+#' @details \code{hist.width} and \code{hist.height} are defined in NDC, meaning that
+#' the height and width are percentages of the overall dimensions of the device plot. Default parameters produce
+#' a histogram that covers 15\% (on width) and 30\% (on height) of the overall device plot.
+#' @seealso \link{getBreaks}, \link{choroLayer}, \link{legendChoro}.
+#' @importFrom graphics grconvertX grconvertY hist axis
+#' @examples 
+#' library(sf)
+#' mtq <- st_read(system.file("gpkg/mtq.gpkg", package="cartography"))
+#' # Population density
+#' mtq$POPDENS <- 1e6 * mtq$POP / st_area(x = mtq)
+#' 
+#' choroLayer(x = mtq, var = "POPDENS",
+#'           border = "grey40",
+#'           legend.pos = "topright", legend.values.rnd = 0,
+#'           legend.title.txt = "Population Density\n(people per km2)")
+#' maphist(v = mtq$POPDENS)
+#' @export
+maphist <- function(v,breaks = NULL, method = "quantile", nclass = NULL,
+                    col = NULL, pos = "topleft", hist.width = .15, hist.height = .3,
+                    axes = TRUE, axes.cex = 0.6, frame = FALSE) {
+ 
+  v <- as.vector(na.omit(v))
+  # get the colors and breaks
+  layer <- choro(var = v, distr = breaks, col = col,
+      nclass = nclass, method = method)
+  
+  # exit for none
+  positions <- c("bottomleft", "topleft", "topright", "bottomright",
+                 "left", "right", "top", "bottom", "center", 
+                 "bottomleftextra")
+  if(length(pos) == 1){if(!pos %in% positions){return(invisible())}}
+  
+  # figdim in geo coordinates
+  x1 <- par()$usr[1]
+  x2 <- par()$usr[2]
+  y1 <- par()$usr[3]
+  y2 <- par()$usr[4]
+  
+  # offsets
+  delta1 <- xinch(0.15)
+  delta2 <- delta1 / 2
+  
+  # variables internes
+  hist.width <- max(0.01, min(0.99, hist.width))
+  hist.height <- max(0.01, min(0.99, hist.height))
+  
+  legend_xsize <- (x2 - x1) * hist.width
+  legend_ysize <- (y2 - y1) * hist.height
+  
+  legcoord <- legpos(pos = pos, x1 = x1, x2 = x2, y1 = y1, y2 = y2,
+                     delta1 = delta1, delta2 = delta2,
+                     legend_xsize = legend_xsize,
+                     legend_ysize = legend_ysize)
+  
+  xref <- legcoord$xref
+  yref <- legcoord$yref
+  
+  # Frame
+  if (frame == TRUE){
+    rect(xref - delta1, yref - delta1, xref + legend_xsize + delta1 * 2,
+         yref + legend_ysize + delta1 * 2, border = "black",  col="white")
+  }
+  
+  #Guess the bins on hist
+  hbreaks <- min(length(unique(v)), 50) #Max 50 bins
+  bins <- pretty(v, n = hbreaks)
+  intervs <- layer$distr
+  intervs[c(1, length(intervs))] <- c(-Inf, Inf)
+  
+  # get the colors and breaks for bins
+  hist <-  choro(var = bins, distr = intervs, col = col)
+  
+  #Store initial parasm
+  opar <- par(no.readonly = TRUE)
+  
+  #Setting fig params
+  histfig <- c(grconvertX(c(xref - delta1, xref + legend_xsize + delta1 * 2), "user", "ndc"),
+              grconvertY(c(yref - delta1, yref + legend_ysize + delta1 * 2), "user", "ndc"))
+  
+  par(new = TRUE, mgp = c(0, 0, 0), mar = c(1, 1, 1, 1),
+    fig = histfig, cex.axis = axes.cex)
+  
+  hist(x = v, breaks = hbreaks, col = hist$colMap,
+    axes = FALSE, xlab = "", ylab = "",
+    main = "", border = hist$colMap)
+  
+  #Axes control
+  if (axes %in% c(TRUE,"h")){
+    axis(1, line = 0.001, pos = NA, col = NA, col.ticks = "grey20", lwd.ticks = 0)
+    }
+  if (axes %in% c(TRUE,"v")){
+    axis(2, line = 0.01, tck = 1, col = NA, col.ticks = "grey20", lwd.ticks = "1", lwd = 0.1, lty = 3)
+    }
+  par(opar)
+  }
