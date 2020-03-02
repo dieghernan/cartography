0f5938f39b44c1c0dd97b4a469041dd7288afbbd
dieghernan <diego.hernangomezherrero@gmail.com>
2020-03-02 06:41:47 +0100

    Fix tinytest

--- inst/tinytest/test_maphist.R
+++ inst/tinytest/test_maphist.R
@@ -1,4 +1,6 @@
 library(sf)
+
+par(mar=c(0,0,0,0))
 mtq <- st_read(system.file("gpkg/mtq.gpkg", package="cartography"), quiet = TRUE)
 plot(st_geometry(mtq))
 s=sample(seq(1,100),20, replace =TRUE)
@@ -9,12 +11,13 @@ expect_silent(maphist(s,hist.width = 3,hist.height =  5, axes=FALSE, pos="top"))
 expect_silent(maphist(s,title.txt = "ABCde", 
                       title.cex=3, pos="right", frame=TRUE))
 
-plot(st_geometry(mtq))
+
 centr=st_coordinates(st_centroid(st_union(mtq)))
+
 expect_silent(maphist(s, col=colors(),
-                      hist.width = 0.1,
-                      hist.height = 0.1,
-                       pos=centr,
+                      hist.width = 0.15,
+                      hist.height = 0.2,
+                      pos=centr,
                       frame=TRUE))
 expect_silent(maphist(s,pos="nada"))
 
